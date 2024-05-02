f241e6f398983b261e75a8c821385b1499a2b13c
Callum Ormsby <callum@hirestreetuk.com>
2024-05-02 17:52:35 +0100

    Name the routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::get('shops', [ShopController::class, 'index']);
-    Route::get('shops/near/{postcode}', [ShopController::class, 'nearPostcode']);
-    Route::get('shops/delivery/{postcode}', [ShopController::class, 'deliveryPostcode']);
-    Route::post('shops', [ShopController::class, 'store']);
+    Route::get('shops', [ShopController::class, 'index'])->name('shops.index');
+    Route::get('shops/near/{postcode}', [ShopController::class, 'nearPostcode'])->name('shops.near.postcode');
+    Route::get('shops/delivery/{postcode}', [ShopController::class, 'deliveryPostcode'])->name('shops.delivery.postcode');
+    Route::post('shops', [ShopController::class, 'store'])->name('shops.store');
 });
