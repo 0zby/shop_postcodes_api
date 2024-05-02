412db90c4874317137daae0fbd355657cce9e078
Callum Ormsby <callum@hirestreetuk.com>
2024-05-02 16:24:02 +0100

    New routes for the shops that deliver or are nearby

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,17 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Postcode;
 use App\Models\Shop;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
         return response()->json(Shop::all());
     }
 
-    public function store(Request $request)
+    /**
+     * Get all shops near a postcode.
+     *
+     * @param string $postcode The postcode to find shops near.
+     * @return JsonResponse The shops near the postcode.
+     */
+    public function nearPostcode(string $postcode): JsonResponse
+    {
+        // This is not optimal - it feels like with a bit of maths we can query the database at least for a rough estimate of the shops that are near a postcode.
+        // Do custom exception for postcode not found and respond accordingly.
+        $postcode = Postcode::where('postcode', $postcode)->firstOrFail();
+        $shops = Shop::all();
+        $nearShops = $shops->filter(function ($shop) use ($postcode) {
+            $distance = $shop->distanceTo($postcode->latitude, $postcode->longitude);
+            // I've set distance to 3000 meters as a rough estimate of what "near" means but perhaps we should configure it or accept it in the request.
+            return $distance <= 3000;
+        });
+
+        return response()->json($nearShops);
+    }
+
+    /**
+     * Get all shops that deliver to a postcode.
+     *
+     * @param string $postcode The postcode to find shops that deliver to.
+     * @return JsonResponse The shops that deliver to the postcode.
+     */
+    public function deliveryPostcode(string $postcodeString): JsonResponse
+    {
+        $preparedPostcode = $this->preparePostcode($postcodeString);
+
+        $postcode = Postcode::where('postcode', $preparedPostcode)->firstOrFail();
+
+        // Again it feels like with some maths we can stop querying all shops and just query the ones that are within the delivery range or at least a good estimate.
+        $shops = Shop::all();
+        $deliveryShops = $shops->filter(function ($shop) use ($postcode) {
+            $distance = $shop->distanceTo($postcode->latitude, $postcode->longitude);
+            return $distance <= $shop->max_delivery_meters;
+        });
+
+        return response()->json($deliveryShops);
+    }
+
+    /**
+     * Store a new shop.
+     *
+     * @param Request $request The request containing the shop data.
+     * @return JsonResponse The stored shop.
+     */
+    public function store(Request $request): JsonResponse
     {
         $request->validate([
             'name' => 'required|string',
@@ -33,5 +85,19 @@ class ShopController extends Controller
         $shop->save();
 
         return response()->json($shop, 201);
+    }
+
+    /**
+     * Prepare the postcode for use.
+     *
+     * @param string $postcode The postcode to prepare.
+     * @return string The prepared postcode.
+     */
+    private function preparePostcode(string $postcode): string
+    {
+        $formattedPostcode = Str::remove(' ', $postcode);
+        $formattedPostcode = Str::upper($formattedPostcode);
+
+        return $formattedPostcode;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
     Route::get('shops', [ShopController::class, 'index']);
+    Route::get('shops/near/{postcode}', [ShopController::class, 'nearPostcode']);
+    Route::get('shops/delivery/{postcode}', [ShopController::class, 'deliveryPostcode']);
     Route::post('shops', [ShopController::class, 'store']);
 });
